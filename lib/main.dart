789ce603e556e7f6a92da39cbdcf4665b4eb6f4f
Mirror83 <glenochieng045@gmail.com>
2024-04-27 10:17:37 +0300

    Allowed for posting of urls to the CleanUrl API and displaying of the shortened results.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import "package:http/http.dart" as http;
+import 'package:url_launcher/link.dart';
 
 void main() {
   runApp(const MyApp());
@@ -81,6 +85,22 @@ class HeroSection extends StatelessWidget {
   }
 }
 
+class ShortenedLink {
+  final String longLink;
+  final String resultUrl;
+  final DateTime createdAt = DateTime.now();
+
+  ShortenedLink({required this.longLink, required this.resultUrl});
+
+  factory ShortenedLink.fromJson(Map<String, dynamic> json, String longLink) {
+    return switch (json) {
+      {'result_url': String resultUrl} =>
+        ShortenedLink(longLink: longLink, resultUrl: resultUrl),
+      _ => throw const FormatException("Failed to shorten URL"),
+    };
+  }
+}
+
 class LinkForm extends StatefulWidget {
   const LinkForm({super.key});
 
@@ -89,32 +109,136 @@ class LinkForm extends StatefulWidget {
 }
 
 class _LinkFormState extends State<LinkForm> {
+  var longLink = "";
+  var isLoading = false;
+  final shortenedLinks = <ShortenedLink>{};
+
+  void shortenLink() async {
+    try {
+      setState(() {
+        isLoading = true;
+      });
+      final shortenedLink = await fetchShortenedLink();
+      setState(() {
+        shortenedLinks.add(shortenedLink);
+      });
+    } catch (exception) {
+      return;
+    } finally {
+      setState(() {
+        isLoading = false;
+      });
+    }
+  }
+
+  Future<ShortenedLink> fetchShortenedLink() async {
+    final response = await http
+        .post(Uri.parse("https://cleanuri.com/api/v1/shorten"), body: {
+      "url": longLink,
+    });
+
+    if (response.statusCode == 200) {
+      return ShortenedLink.fromJson(
+          jsonDecode(response.body) as Map<String, dynamic>, longLink);
+    } else {
+      throw Exception("Failed to shorten URL");
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
-    return Padding(
-      padding: const EdgeInsets.all(32.0),
-      child: Card(
-        color: theme.colorScheme.surfaceVariant,
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Stack(children: <Widget>[
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                const TextField(
-                  decoration: InputDecoration(
-                    hintText: "Shorten a link here...",
+    return Column(children: [
+      Padding(
+        padding: const EdgeInsets.all(32.0),
+        child: Card(
+          color: theme.colorScheme.surfaceVariant,
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Stack(children: <Widget>[
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  TextField(
+                    onChanged: (value) => setState(() {
+                      longLink = value;
+                    }),
+                    decoration: const InputDecoration(
+                      hintText: "Shorten a link here...",
+                    ),
                   ),
-                ),
-                const SizedBox(height: 16),
-                ElevatedButton(
-                  onPressed: () {},
-                  child: const Text("Shorten It!"),
-                ),
-              ],
+                  const SizedBox(height: 16),
+                  ElevatedButton(
+                    onPressed: isLoading
+                        ? null
+                        : () {
+                            shortenLink();
+                          },
+                    child: const Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text("Shorten It!"),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ]),
+          ),
+        ),
+      ),
+      Column(
+        children: shortenedLinks
+            .map((shortenedLink) => Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: LinkCard(shortenedLink: shortenedLink),
+                ))
+            .toList(),
+      ),
+    ]);
+  }
+}
+
+class LinkCard extends StatelessWidget {
+  final ShortenedLink shortenedLink;
+  const LinkCard({
+    super.key,
+    required this.shortenedLink,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final ThemeData theme = Theme.of(context);
+
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.all(32.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(shortenedLink.longLink),
+            const Divider(
+              thickness: 2,
             ),
-          ]),
+            const SizedBox(
+              height: 16,
+            ),
+            Link(
+                uri: Uri.parse(shortenedLink.resultUrl),
+                builder: (context, followLink) => TextButton(
+                      onPressed: followLink,
+                      child: Text(
+                        shortenedLink.resultUrl,
+                        style: theme.textTheme.bodyLarge!.copyWith(
+                            color: theme.primaryColor,
+                            decoration: TextDecoration.underline,
+                            decorationColor: theme.primaryColor),
+                      ),
+                    )),
+            const SizedBox(height: 16),
+            OutlinedButton(
+                onPressed: () {}, child: const Text("Copy to clipboard"))
+          ],
         ),
       ),
     );
